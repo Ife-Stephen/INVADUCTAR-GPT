2f3e335089a271c03760a1250b74cf42e58135ef
Ife Stephen <105988938+Ife-Stephen@users.noreply.github.com>
2025-10-30 22:29:53 +0100

    Update ingest_pdfs.py

--- ingest_pdfs.py
+++ ingest_pdfs.py
@@ -3,7 +3,7 @@ import re
 import shutil
 from typing import List, Tuple
 from langchain_core.documents import Document
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from pypdf import PdfReader
@@ -79,4 +79,5 @@ if __name__ == "__main__":
         print("✅ RAG index is ready for use.")
     except Exception as e:
         print(f"❌ Failed to rebuild RAG: {e}")
+
 
